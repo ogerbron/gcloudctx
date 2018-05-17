c1304d2184690f44e3f8a1330c150f6c912c1e63
Olivier Gerbron <olivier.gerbron@chauffeur-prive.com>
2018-05-17 18:03:37 +0200

    Renamed command to gcloudctx

--- completion/gcloudctx.bash
+++ completion/gcloudctx.bash
@@ -1,9 +1,9 @@
-_gcloud_mp()
+_gcloudctx()
 {
   local curr_arg;
   curr_arg=${COMP_WORDS[COMP_CWORD]}
   COMPREPLY=( $(compgen -W "- $(gcloud config configurations list --format="table(name)" | grep -v NAME)" -- $curr_arg ) );
 }
 
-complete -F _gcloud_mp gcloudctx
+complete -F _gcloudctx gcloudctx
 
